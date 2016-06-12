45e3d09edad0c611c039f6656ae17cf03af5e613
RIdotCOM <richardivan.com@gmail.com>
2016-06-12 18:33:51 +0200

    refactor(test): add mock to mq

--- tests/app/js/containers/toolbar.spec.js
+++ tests/app/js/containers/toolbar.spec.js
@@ -1,6 +1,16 @@
 'use strict'
 
-import '../../js/misc'
+// import '../../js/misc'
+
+// console.log(global.window)
+// if (!global.window) {
+//   global.window = require('mithril/test-utils/domMock.js')()
+//   global.window = Object.assign(global.window, require('mithril/test-utils/pushStateMock')())
+// }
+// console.log(global.window)
+//
+// global.window = require('mithril/test-utils/domMock.js')()
+// global.window = Object.assign(global.window, require('mithril/test-utils/pushStateMock')())
 
 import o from '../../../../ospec/ospec.js'
 import mq from 'mithril-query'

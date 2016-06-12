3eaaf50e6b59e9672f3e33d1b0fdaebb103c0261
RIdotCOM <richardivan.com@gmail.com>
2016-06-12 19:35:56 +0200

    test(Toolbar): remove toolbar tests

--- tests/app/js/containers/toolbar.spec.js
+++ tests/app/js/containers/toolbar.spec.js
@@ -14,29 +14,29 @@
 
 import o from '../../../../ospec/ospec.js'
 import mq from 'mithril-query'
-
-import Toolbar from '../../../../app/js/containers/Toolbar'
-
-o.spec('toolbar container', () => {
-  o.spec('structure', () => {
-    o('toolbar on geo root has h1', () => {
-      // o(true).equals(true)
-      o(mq(Toolbar, { route: 'geo' }).has('h1')).equals(true)
-    })
-
-    o('toolbar on data root has h1', () => {
-      // o(true).equals(true)
-      o(mq(Toolbar, { route: 'data' }).has('h1')).equals(true)
-    })
-
-    o('toolbar on graph root has h1', () => {
-      // o(true).equals(true)
-      o(mq(Toolbar, { route: 'graph' }).has('h1')).equals(true)
-    })
-  })
-  // o.spec('behaviour', () => {
-  //   o('behaves', () => {
-  //     o(true).equals(true)
-  //   })
-  // })
-})
+//
+// import Toolbar from '../../../../app/js/containers/Toolbar'
+//
+// o.spec('toolbar container', () => {
+//   o.spec('structure', () => {
+//     o('toolbar on geo root has h1', () => {
+//       // o(true).equals(true)
+//       o(mq(Toolbar, { route: 'geo' }).has('h1')).equals(true)
+//     })
+//
+//     o('toolbar on data root has h1', () => {
+//       // o(true).equals(true)
+//       o(mq(Toolbar, { route: 'data' }).has('h1')).equals(true)
+//     })
+//
+//     o('toolbar on graph root has h1', () => {
+//       // o(true).equals(true)
+//       o(mq(Toolbar, { route: 'graph' }).has('h1')).equals(true)
+//     })
+//   })
+//   // o.spec('behaviour', () => {
+//   //   o('behaves', () => {
+//   //     o(true).equals(true)
+//   //   })
+//   // })
+// })

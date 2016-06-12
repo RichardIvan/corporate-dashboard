87f21ff48800b59793f7b0d10aa1f44b61cf335b
RIdotCOM <richardivan.com@gmail.com>
2016-06-12 19:16:01 +0200

    fix(Toolbar): revert toolbar to previous state

--- app/js/containers/Toolbar.js
+++ app/js/containers/Toolbar.js
@@ -1,18 +1,19 @@
+/* @flow */
+
 // this is a smart component
 
 import m from 'mithril'
-// import m from 'mithril/render/hyperscript'
 // import classNames from 'classnames'
-// import ToolbarComponent from '../components/Toolbar'
+import ToolbarComponent from '../components/Toolbar'
 // import {getMessages} from '../selectors'
 // import {clearMessage} from '../actions'
 // import styles from './style.scss'
 
 const Toolbar = {
-  view() {
+  view(vnode) {
     // const state = store.getState();
     // console.log(vnode)
-    return m('h1')
+    return m(ToolbarComponent)
 
     // return (
     //   <ToolbarComponent className={classNames(styles.messages, className)}

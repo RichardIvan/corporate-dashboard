9b312a0be77e23cdb62dea1ecfd3207c40de6802
RIdotCOM <richardivan.com@gmail.com>
2016-06-20 13:29:08 +0200

    feat(mockaroo): add implementation for fillng ID, Location, and Opening Timestamp

--- server/src/helpers/fillers.js
+++ server/src/helpers/fillers.js
@@ -1,13 +1,28 @@
+/* @flow */
+'use strict'
+
+import uuid from 'uuid-v4'
+import sample from 'lodash/sample'
+import { locations } from '../data'
+
 export function fillIDs (json) {
-  return json
+  return json.map((issue) => ({ ...issue, id: uuid() }))
 }
 
 export function fillLocation(json) {
-  return json
+  return json.map((issue) => ({ ...issue, location: sample(locations) }))
 }
 
 export function fillOpeningTimestamp(json) {
-  return json
+  const date = new Date()
+  const now = new Date()
+  const past = new Date(now.setFullYear((date.getFullYear() - 1)))
+
+  const diff = date.getTime() - past.getTime()
+  const newDiff = diff * Math.random()
+  const newDate = new Date(past.getTime() + newDiff)
+
+  return json.map((issue) => ({ ...issue, opening_timestamp: newDate.getTime() }))
 }
 export function fillClosingTimestamp(json) {
   return json

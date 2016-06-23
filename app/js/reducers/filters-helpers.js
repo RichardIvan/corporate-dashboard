7772314724322c734e5386912e4e0a222c20d7f6
RIdotCOM <richardivan.com@gmail.com>
2016-06-23 14:35:42 +0200

    feat(Filters): add reset action hander

---
+++ app/js/reducers/filters-helpers.js
@@ -0,0 +1,48 @@
+/* @flow */
+'use strict'
+
+import { Map } from 'immutable'
+
+import {
+  SET_FILTER,
+  NAME_TYPE,
+  EMAIL_TYPE,
+  OPENING_TIMESTAMP_TYPE,
+  CLOSING_TIMESTAMP_TYPE,
+  EMPLOYEE_TYPE,
+  OPEN_STATUS_TYPE,
+  LOCATION_TYPE,
+} from '../actions'
+
+export const initialState = Map({
+  [NAME_TYPE]:  Map({
+    active: false,
+    by: '',
+  }),
+  [EMAIL_TYPE]: Map({
+    active: false,
+    by: '',
+  }),
+  [OPENING_TIMESTAMP_TYPE]: Map({
+    active: false,
+    from: -1,
+    to: -1,
+  }),
+  [CLOSING_TIMESTAMP_TYPE]: Map({
+    active: false,
+    from: -1,
+    to: -1,
+  }),
+  [EMPLOYEE_TYPE]: Map({
+    active: false,
+    by: '',
+  }),
+  [OPEN_STATUS_TYPE]: Map({
+    active: false,
+    by: true,
+  }),
+  [LOCATION_TYPE]: Map({
+    active: false,
+    by: '',
+  }),
+})

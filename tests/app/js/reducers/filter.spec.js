74b9983778d1da82e89bfd3fcf7dced3d389712c
RIdotCOM <richardivan.com@gmail.com>
2016-06-23 13:53:39 +0200

    test(Filters Reducer): add test that checks if the state has all required types of filters

--- tests/app/js/reducers/filter.spec.js
+++ tests/app/js/reducers/filter.spec.js
@@ -20,7 +20,7 @@ import {
 import reducer from '../../../../app/js/reducers/filters'
 
 describe('Filter Reducer', () => {
-  it.only('should return a Map', () => {
+  it('should return a Map', () => {
     const state = undefined
     const action = {
       type: 'NONE',
@@ -29,7 +29,19 @@ describe('Filter Reducer', () => {
     expect(Map.isMap(newState)).toBe(true)
   })
 
-  it('should containt all types in map', () => {
+  it.only('should containt all types in map', () => {
+    const state = undefined
+    const action = {
+      type: 'NONE',
+    }
+    const newState = reducer(state, action)
+    expect(newState.has(NAME_TYPE)).toBe(true)
+    expect(newState.has(EMAIL_TYPE)).toBe(true)
+    expect(newState.has(OPENING_TIMESTAMP_TYPE)).toBe(true)
+    expect(newState.has(CLOSING_TIMESTAMP_TYPE)).toBe(true)
+    expect(newState.has(EMPLOYEE_TYPE)).toBe(true)
+    expect(newState.has(OPEN_STATUS_TYPE)).toBe(true)
+    expect(newState.has(LOCATION_TYPE)).toBe(true)
 
   })
 })

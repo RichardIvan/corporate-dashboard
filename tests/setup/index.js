6f685c468b953e8de74580ec8962bd1c4fbfdd25
RIdotCOM <richardivan.com@gmail.com>
2016-07-24 10:43:48 +0200

    test(build fair fix): remove babel-register from test setup file
    test is failing for double use of babel-register

--- tests/setup/index.js
+++ tests/setup/index.js
@@ -1,18 +1,18 @@
 require('ignore-styles')
 // require('babel-core/register')
-require('babel-register')({
+// require('babel-register')({
   // Optional ignore regex - if any filenames **do** match this regex then they
   // aren't compiled.
   // ignore: /regex/,
 
   // Ignore can also be specified as a function.
-  ignore: function (filename) {
-    if (filename.includes('/node_modules/rereduce')) {
-      return false
-    } else {
-      return true
-    }
-  }
+  // ignore: function (filename) {
+  //   if (filename.includes('/node_modules/rereduce')) {
+  //     return false
+  //   } else {
+  //     return true
+  //   }
+  // }
 
   // Optional only regex - if any filenames **don't** match this regex then they
   // aren't compiled
@@ -21,7 +21,7 @@ require('babel-register')({
   // Setting this will remove the currently hooked extensions of .es6, `.es`, `.jsx`
   // and .js so you'll have to add them back if you want them to be used again.
   // extensions: [".es6", ".es", ".jsx", ".js"]
-})
+// })
 
 global.window = require('mithril/test-utils/domMock.js')()
 global.window = Object.assign(global.window, require('mithril/test-utils/pushStateMock')())
